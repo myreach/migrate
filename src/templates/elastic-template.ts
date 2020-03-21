772abbe256bb8f4103623407d95cb2e70ceeaec8
Gonzalo De Benito <gdebenitocassado@gmail.com>
2020-03-22 00:49:33 +0100

    refactored templates

---
+++ src/templates/elastic-template.ts
@@ -0,0 +1,31 @@
+import { MigrationTemplate } from "./migration-template";
+
+export class ElasticTemplate implements MigrationTemplate {
+  constructor(
+    public name: string,
+    public timestamp = Date.now(),
+    public className: string
+  ) {}
+
+  template = () => `import { ElasticSearchMigration } from '@myreach/migrate/src/migration/types/neo4j-migration';
+import { Client } from '@elastic/elasticsearch';
+
+export default class ${this.className} extends ElasticSearchMigration {
+    name = '${this.name}';
+    timestamp = ${this.timestamp};
+
+    // Necessary for ElasticSearchMigration class configuration
+    constructor() {
+        super();
+    }
+
+    public async up(client: Client) {
+        // Your code here
+    }
+
+    public async down(client: Client) {
+        // Your code here
+    }
+}
+`;
+}
--- src/templates/migration-template.ts
+++ src/templates/migration-template.ts
@@ -1,14 +1,6 @@
-export const makeTemplate = (name: string, timestamp: number): string => `
-import { MigrationInterface } from '@reach/es-migrate/src/migration/migration-interface';
-import { Client } from '@elastic/elasticsearch';
-
-export default class ${name + timestamp} implements MigrationInterface {
-	name = '${name}';
-	timestamp = ${timestamp};
-
-	public async up(client: Client) {}
-
-	public async down(client: Client) {}
+export interface MigrationTemplate {
+  name: string;
+  className: string;
+  timestamp: number;
+  template(): string;
 }
-
-`;
