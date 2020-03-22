948b9885b1f3bf9dbeddb4b54419e1ccb3589d1a
Gonzalo De Benito <gdebenitocassado@gmail.com>
2020-03-22 10:58:42 +0100

    install readline-promise

--- src/utils/readline-promise.ts
+++
@@ -1,9 +0,0 @@
-export const readlinePromise = (readline: any) => {
-  readline.questionAsync = (question: string) =>
-    new Promise((res, rej) => {
-      readline.question(question, (answer: string) => {
-        res(answer);
-      });
-    });
-  return readline;
-};
